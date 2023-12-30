77cca6952077f4e6af0dfddd36732944a851d7b5
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-29 22:04:22 -0500

    Update AppLog.php

--- src/AppLog.php
+++ src/AppLog.php
@@ -9,6 +9,9 @@ class AppLog
 		unset($argv[0]);
 		$url = json_decode(file_get_contents(__DIR__ . "/../config/disclog.json"), true)["app-log"];
 		if (count($argv)) return Webhook::send($url, substr(implode(" ", $argv), 0, 2000));
-		while ($line = trim(fgets(STDIN))) Webhook::send($url, substr($line, 0, 2000));
+		while (($line = fgets(STDIN)) !== false) {
+			$line = trim($line);
+			Webhook::send($url, substr($line, 0, 2000));
+		}
 	}
 }
